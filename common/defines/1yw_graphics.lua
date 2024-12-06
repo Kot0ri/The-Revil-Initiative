3df8e7d1cf71fcde1b9cf2d6f692ac0f877c3e9f
Kot0ri <111668565+Kot0ri@users.noreply.github.com>
2024-12-05 20:58:14 -0500

    oop

--- common/defines/1yw_graphics.lua
+++
@@ -1,2 +0,0 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {1000, 1000, 1000}
-NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 40.0
--- common/defines/ief_defines.lua
+++
@@ -1,2 +0,0 @@
-NDefines.NBuildings.MAX_SHARED_SLOTS = 72;				-- Max slots shared by factories
-NDefines.NBuildings.MAX_BUILDING_LEVELS = 72;			-- Max levels a building can have.

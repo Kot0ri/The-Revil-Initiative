904705db003ac2fb2006f22517bdeabf030e2e27
Kot0ri <111668565+Kot0ri@users.noreply.github.com>
2025-12-23 18:25:21 -0500

    buildings

--- common/defines/ief_defines.lua
+++ common/defines/ief_defines.lua
@@ -1,2 +1,3 @@
 NDefines.NBuildings.MAX_SHARED_SLOTS = 72;				-- Max slots shared by factories
 NDefines.NBuildings.MAX_BUILDING_LEVELS = 72;			-- Max levels a building can have.
+NDefines.NBuildings.DESTRUCTION_COOLDOWN_IN_WAR = 1;	-- Number of days cooldown between
